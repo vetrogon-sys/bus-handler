8f7bf14d56213667d80536440c0fee93a4649edb
Kostya <60735936+vetrogon-sys@users.noreply.github.com>
2023-07-12 21:32:32 +0300

    initial commit

---
+++ src/main/java/com/example/busticketplatform/BusTicketPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.busticketplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/busticketplatform/BusTicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busticketplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
